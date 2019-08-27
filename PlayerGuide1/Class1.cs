99e2c4d674c4472ec9a2cb9f7e64dd9cf09a5fed
{dp7jm} <{dpjm@chef.net}>
2019-08-27 02:29:57 +0200

    changed name of a variable to more appropriate

--- PlayerGuide1/Class1.cs
+++ PlayerGuide1/Class1.cs
@@ -10,14 +10,14 @@ namespace PlayerGuide1
     {
         public static int binaryArrayToNumber(int[] BinaryArray)
         {
-            var bitNumber = 0;
+            var convertedNumber = 0;
 
             for (int i = 0; i < BinaryArray.Length; i++)
             {
-                bitNumber += BinaryArray[BinaryArray.Length - i - 1] * (int)Math.Pow(2, i);
+                convertedNumber += BinaryArray[BinaryArray.Length - i - 1] * (int)Math.Pow(2, i);
             }
-            Console.WriteLine(bitNumber);
-            return bitNumber;
+            Console.WriteLine(convertedNumber);
+            return convertedNumber;
             
         }
     }
